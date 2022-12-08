493715799bead771e1f3b80058b9ff2d06c94dfb
Afonso Azaruja <afonso.azaruja@mailbox.org>
2022-12-08 14:42:08 +0000

    New Methods

--- proj1.cpp
+++ proj1.cpp
@@ -26,6 +26,41 @@ void print_matrix(int n, int m, matrix &mat) {
     }
 }
 
+int compute_max_area_by_vec(int n, vector<int> area) {
+    int max = 0;
+    for(int i = 0; i < n; i++) {
+        int square = area.at(i);
+        if((square <= n) && (i+square <= n) && (square > max)) max = square;
+    }
+    return max;
+}
+
+int compute_max_by_matrix(int n, int m, matrix &mat) {
+    int big = 0;
+    int times = 0;
+    for(int i = 0; i < n-1; i++) {
+        for(int j = 0; j < m-1; j++) {
+            int size = 1;
+            bool valid = true;
+            if((mat.at(i).at(j) == 1) && mat.at(i+1).at(j+1) == 1) {
+                for(int iter = 1; (i+iter < n) && (j+iter < m) && (valid == true); iter++) {
+                    for(int k = i+iter; k >= i; k--) {
+                        if(mat.at(k).at(j+iter) == 0) {valid = false;}
+                    }
+                    for(int l = j+iter; l >= j; l--) {
+                        if(mat.at(i+iter).at(l) == 0) {valid = false;}
+                    }
+                    if(valid) size++;
+                }
+            }
+            if(size > big) {big = size; times = 1;}
+            else if(size == big) times++;
+        }
+    }
+    cout << "\nsize:" << big << "\ntimes:" << times;
+    return big;
+}
+
 void compute_areas(int n, int m, matrix &mat) {
     int biggest_square = 0;
     int times = 0;
@@ -71,6 +106,6 @@ int main() {
  
     initialize_matrix(n, m, area, mat);
     print_matrix(n, m, mat);
-    compute_areas(n, m, mat);
+    //compute_areas(n, m, mat);
     return 0;
 }
