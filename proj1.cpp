aa277ef4ad5359a54a860b0743d5c2825e4ed918
Afonso Azaruja <afonso.azaruja@mailbox.org>
2022-12-08 19:29:18 +0000

    Added recursive functionality.

--- proj1.cpp
+++ proj1.cpp
@@ -35,6 +35,26 @@ int compute_max_area_by_vec(int n, vector<int> area) {
     return max;
 }
 
+matrix copy_matrix(int n, int m, matrix &mat) {
+    matrix copy = {};
+    for(int i = 0; i<n; i++) {
+        vector<int> vec = {};
+        for(int j = 0; j<m; j++) {
+            vec.push_back(mat.at(i).at(j));
+        }
+        copy.push_back(vec);
+    }
+    return copy;
+}
+
+void clear_area(matrix &mat, int l, int c, int size) {
+    for(int i = l; i < l+size-1; i++) { 
+        for(int j = c; j < c+size-1; j++) {
+            mat.at(i).at(j) = 0;
+        }
+    }
+}
+
 int compute_max_by_matrix(int n, int m, matrix &mat, int max_size) {
     int big = 0;
     int times = 0;
@@ -43,49 +63,31 @@ int compute_max_by_matrix(int n, int m, matrix &mat, int max_size) {
             int size = 1;
             bool valid = true;
             if((mat.at(i).at(j) == 1) && mat.at(i+1).at(j+1) == 1) {
-                for(int iter = 1; (i+iter < n) && (j+iter < m) && (valid == true) && (iter < max_size); iter++) {
+                for(int iter = 1; (i+iter < n) && (j+iter < m) && (valid == true); iter++) {
                     for(int k = i+iter; k >= i; k--) {
                         if(mat.at(k).at(j+iter) == 0) {valid = false;}
                     }
                     for(int l = j+iter; l >= j; l--) {
                         if(mat.at(i+iter).at(l) == 0) {valid = false;}
                     }
-                    if(valid) size++;
+                    if(valid) {
+                        size++;
+                        if(size == max_size) {
+                            matrix aux = copy_matrix(n, m, mat);
+                            clear_area(aux, i, j, size);
+                            print_matrix(n, m, aux);
+                            return compute_max_by_matrix(n, m, aux, max_size) + 1;
+                        }
+                    }
                 }
             }
             if(size > big) {big = size; times = 1;}
             else if(size == big) times++;
         }
     }
-    cout << "\nsize:" << big << "\ntimes:" << times;
-    return big;
-}
-
-void compute_areas(int n, int m, matrix &mat) {
-    int biggest_square = 0;
-    int times = 0;
-    for(int i = 0; i < n; i++) {
-        int size = 0;
-        int col = 0;
-        for(int j = 0; (j < m) && (mat.at(i).at(j) > 0); j++) {
-            if(i+j-col < n) {
-                size++;
-            }
-            else {
-                if(size > biggest_square) {biggest_square = size; times = 1;}
-                else if(size == biggest_square) {times++;}
-                if(j < m) {
-                    j -= size;
-                    size = 0;
-                    col++;
-                    
-                }
-            }
-        }
-        if(size > biggest_square) {biggest_square = size; times = 1;}
-        else if(size == biggest_square) times++;
-    }
-    cout << "\nsize:" << biggest_square << "\ntimes:" << times;
+    //cout << "\nsize:" << big << "\ntimes:" << times;
+    if(max_size < 1) return 0;
+    else return compute_max_by_matrix(n, m, mat, max_size-1) + 1;
 }
 
 int main() {
@@ -102,6 +104,7 @@ int main() {
     initialize_matrix(n, m, area, mat);
     print_matrix(n, m, mat);
     int initial_max = compute_max_area_by_vec(n, area);
-    compute_max_by_matrix(n, m, mat, initial_max);
+    int ways = compute_max_by_matrix(n, m, mat, initial_max);
+    cout << "\nPossibilidades: " << ways; 
     return 0;
 }
