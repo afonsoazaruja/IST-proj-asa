c1963ef04936a8314cebcf930fd9b6dac63ceb8a
Afonso Azaruja <afonso.azaruja@mailbox.org>
2022-12-13 18:33:48 +0000

    New recursive method. 1600 points.

--- Makefile
+++ Makefile
@@ -1,12 +1,17 @@
-CFLAGS = -O3 -std=c++11 -Wall
+CFLAGS = -O3 -std=c++11 -Wall -g
 
 program:
+	g++ $(CFLAGS) proj1.cpp -lm -o program
+
+1:
 	g++ $(CFLAGS) projOriginal.cpp -lm -o program
 
 clean:
-	rm -f program
+	rm -f program program1
 
 exec:
 	./program
 
 run: clean program exec
+
+new: clean 1 exec
--- proj1.cpp
+++ proj1.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
 
-map<vector<int>, long> mem;
+unordered_map<long, unsigned long> mem;
 
-bool only_square_1(int n, vector<int> &area) {
+bool onlyOneByOne(int n, vector<int> &area) {
     int l = area[0];
     for(int i = 1; i < n; i++) {
         if(area[i] >= l && l > 1) {return false;}
@@ -15,12 +15,29 @@ bool only_square_1(int n, vector<int> &area) {
     return true;
 }
 
-long compute(int n, vector<int> &area, int lin, int square) {
-    if(square == -1) {
-        int col = area[0];
-        int consecutive = 1;
-        bool previous = (col == area[1]);
-        bool next = false;
+unsigned long hashKey(vector<int> &vec) {
+    unsigned long key = 0;
+    for(int i : vec) {
+        key += i;
+        key *= 10;
+    }
+    return key;
+}
+
+unsigned long solve(int n, vector<int> &area) {
+    if(onlyOneByOne(n, area)) return 1;
+
+    unsigned long total = 0;
+
+    unsigned long key = hashKey(area);
+    
+    if(mem.count(key)) total = mem[key];
+    
+    else {
+        int square;
+        int lin = 0;
+        int col = area[0]; int consecutive = 1;
+        bool previous = (col == area[1]); bool next = false;
         for(int i = 1; i < n; i++) {
             int sq = area[i];
             if(next == true) {previous = true; next = false;}
@@ -36,45 +53,24 @@ long compute(int n, vector<int> &area, int lin, int square) {
         }
         if(col > consecutive) square = consecutive;
         else square = col;
+        while(square > 0) {
+            for(int i = lin; i < lin + square; i++) area[i] -= square;
+            total += solve(n, area);
+            for(int i = lin; i < lin + square; i++) area[i] += square;
+            square--;
+        }
+        mem[key] = total;
     }
-    
-    for(int i = lin; i < lin + square; i++) {
-        area[i] -= square;
-    }
-
-    if(mem.count(area) == 1) {
-        if(square == 1) return mem[area];//altered
-        long res = mem[area];
-        for(int i = lin; i < lin + square; i++) area[i] += square;
-        mem[area] = compute(n, area, lin, square-1); //original
-        return mem[area] + res;
-    }
-    if(only_square_1(n, area)) {
-        if(square == 1) return 1;
-        for(int i = lin; i < lin + square; i++) area[i] += square;
-        mem[area] = compute(n, area, lin, square-1);
-        return mem[area] + 1;
-    }
-    if(square == 1) {
-        mem[area] = compute(n, area, 0, -1);
-        return mem[area];
-    }
-    mem[area] = compute(n, area, lin, square-1);
-    long res = mem[area];
-    for(int i = lin; i < lin + square; i++) area[i] += square;
-    mem[area] = compute(n, area, 0, -1);
-    return mem[area] + res;
+    return total;
 }
 
 int main() {
     int n, m;
     cin >> n >> m;
     vector<int> area(n);
-    for(int i = 0; i < n; i++) {
-        cin >> area[i];
-    }
+    for(int i = 0; i < n; i++) cin >> area[i];
     if(n == 0 || area[n-1] == 0) cout << 0 << endl;
-    else if(only_square_1(n, area)) cout << 1 << endl;
-    else cout << compute(n, area, 0, -1) << endl;
+    else if(onlyOneByOne(n, area)) cout << 1 << endl;
+    else cout << solve(n, area) << endl;
     return 0;
 }
