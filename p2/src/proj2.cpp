1ea251f151af05856ea4bf27589f5c528141bc84
Afonso Azaruja <afonso.azaruja@mailbox.org>
2022-12-29 00:41:02 +0000

    Modified queue to map

--- p2/src/proj2.cpp
+++ p2/src/proj2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -7,6 +9,32 @@ typedef struct {
     int v;
     int w;
 } Edge;
+
+// Comparator function to sort pairs
+// according to second value
+bool cmp(pair<int, int>& a, pair<int, int>& b) {
+	return a.second > b.second;
+}
+
+int sort(map<int, int>& M) {
+
+	// Declare vector of pairs
+	vector<pair<int, int>> A;
+
+	// Copy key-value pair from Map
+	// to vector of pairs
+	for (auto& it : M) {
+		A.push_back(it);
+	}
+
+	// Sort using comparator function
+	sort(A.begin(), A.end(), cmp);
+    for (auto& it : A) {
+        cout << it.first << ' ' << it.second << endl;
+    }
+    cout << "---\n";
+    return A[0].first;
+}
 
 Edge make_edge(int x, int y) {
     Edge edge = {x, y};
@@ -18,34 +46,21 @@ void addEdge(vector<Edge> *adj, int u, int v, int w) {
     adj[v-1].push_back(make_edge(u-1, w));
 }
 
-int extract_max(vector<int> key, vector<int> queue, int v) {
-    int max = 0;
-    int i = 0;
-    for (; i < v; i++) {
-        if (queue[i] != 0) {
-            max = i;
-            break;
-        }
-    }
-    for (; i < v; i++) {
-        if (queue[i] != 0 && key[i] > key[max]) {
-            max = i; 
-        }
-    }
-    return max;
+int extract_max(map<int, int> &queue) {
+    return queue[0];
 }
 
-int solve(vector<Edge> *adj, vector<int> key, int v) {
-    vector<int> queue(v, 1);
+int solve(vector<Edge> *adj, vector<int> key, map<int, int> &queue, int v) {
     for (int i = 0; i < v; i++) {
-        int u = extract_max(key, queue, v);
+        int u = sort(queue);
         cout << u << "\n";
         for (Edge j : adj[u]) {
-            if (j.w > key[j.v] && queue[j.v] == 1) {
+            if (queue.count(j.v) > 0 && j.w > key[j.v]) {
                 key[j.v] = j.w;
+                queue[j.v] = j.w;
             }
         }
-        queue[u] = 0; // after checking all edges, remove vertex from queue
+        queue.erase(u); // after checking all edges, remove vertex from queue
     }
     int res = 0;
     for(int i = 0; i < v; i++) {
@@ -57,8 +72,12 @@ int solve(vector<Edge> *adj, vector<int> key, int v) {
 int main() {
     int v, e;
     cin >> v >> e;
-    vector<int> key(v, 0);
     vector<Edge> adj[v];
+    vector<int> key(v, 0);
+    map<int, int> queue;
+    for (int i = 0; i < v; i++) {
+        queue[i] = 0;
+    }
     for (int i = 0; i < e; i++) {
         int u, v, w;
         cin >> u >> v >> w;
@@ -71,6 +90,6 @@ int main() {
         }
         cout << "\n";
     }
-    cout << solve(adj, key, v) << endl;
+    cout << solve(adj, key, queue, v) << endl;
     return 0;
 }
